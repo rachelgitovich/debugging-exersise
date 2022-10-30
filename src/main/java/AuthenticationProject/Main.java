6c7a094e6cdb000e680bf0a6562b082e6dacf0a6
Rachel <rachelibr050@gmail.com>
2022-10-30 16:13:25 +0200

    adding checkIfUserExists

--- src/main/java/AuthenticationProject/Main.java
+++ src/main/java/AuthenticationProject/Main.java
@@ -5,8 +5,5 @@ import AuthenticationProject.UserRepository.UserRepository;
 public class Main {
     public static void main(String[] args) {
 
-//        User user=new User("Rivka","sddfdff","123");
-//        UserRepository.createUser(user);
-
     }
 }
--- src/main/java/AuthenticationProject/UserRepository/UserRepository.java
+++ src/main/java/AuthenticationProject/UserRepository/UserRepository.java
@@ -13,23 +13,26 @@ import java.util.List;
 
 public class UserRepository {
     private static String path = "src/main/java/AuthenticationProject/UserRepository/users.json";
-private static Gson gson=new Gson();
-    private static List<User> fetchUsers(){
+    private static Gson gson = new Gson();
+
+    private static List<User> fetchUsers() {
         JsonReader reader = null;
-        Type USER_TYPE = new TypeToken<List<User>>() {}.getType();
+        Type USER_TYPE = new TypeToken<List<User>>() {
+        }.getType();
         try {
             reader = new JsonReader(new FileReader(path));
         } catch (FileNotFoundException e) {
             throw new RuntimeException("users.json file not found");
         }
         List<User> data = gson.fromJson(reader, USER_TYPE); // contains the whole users list
-        if(data==null){
-            data=new ArrayList<User>();
+        if (data == null) {
+            data = new ArrayList<User>();
         }
         return data;
     }
+
     public static void createUser(User user) {
-        List<User> data=fetchUsers();
+        List<User> data = fetchUsers();
         PrintWriter writer = null;
         data.add(user);
         String usersJson = gson.toJson(data);
@@ -43,9 +46,14 @@ private static Gson gson=new Gson();
         writer.print(usersJson);
         writer.close();
     }
-    public static boolean checkIfEmailExists(String email){
-        List<User> data=fetchUsers();
-        return data.stream().anyMatch(user->user.getEmail().equals(email));
+
+    public static boolean checkIfEmailExists(String email) {
+        List<User> data = fetchUsers();
+        return data.stream().anyMatch(user -> user.getEmail().equals(email));
     }
 
+    public static boolean checkIfUserExists(String email, String password) {
+        List<User> data = fetchUsers();
+        return data.stream().anyMatch(user -> user.getEmail().equals(email) && user.getPassword().equals(password));
+    }
 }
