7bd0dbf10ec9d143ffaa7cd20edf82dea879b7d2
khader-zatari <khaderzatari@gmail.com>
2022-10-31 08:22:43 +0200

    Auth remains validation

--- src/main/java/AuthenticationProject/Main.java
+++ src/main/java/AuthenticationProject/Main.java
@@ -1,5 +1,6 @@
 package AuthenticationProject;
 
+import AuthenticationProject.Controllers.AuthenticationController;
 import AuthenticationProject.Controllers.UserController;
 import AuthenticationProject.UserRepository.UserRepository;
 
@@ -7,6 +8,13 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        UserRepository.checkIfUserExists("sdfs","sdfsf");
+//        UserRepository.checkIfUserExists("sdfs","sdfsf");
+
+//        AuthenticationController.createUser("leon", "leon@gmail.com", "leon123");
+        HashMap<String, String> res = AuthenticationController.logIn("haitham@gmail.com", "password");
+//        System.out.println(res.get("29982e27-105a-403a-8fd3-d6b8756499a3"));
+        boolean test = AuthenticationController.authUser("29982e27-105a-403a-8fd3-d6b8756499a3", res.get("29982e27-105a-403a-8fd3-d6b8756499a3"));
+        System.out.println(test);
+//
     }
 }
